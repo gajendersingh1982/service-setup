86a88f8e111962e16a2573be0aefbc7e149eb046
NokkuPrudhvi2Main <49380061+NokkuPrudhvi@users.noreply.github.com>
2020-05-09 21:15:32 +0530

    Update sg.tf
    added admin sg, openapi-sg

--- sg.tf
+++ sg.tf
@@ -57,3 +57,47 @@ module "db_sg" {
 
   tags        = var.tags #Use common_vars.tf file for tags.Refer Note Below.
 }
+
+  
+ module "openapi_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = format("%s-%s-%s-%s-openapi-sg", var.prefix, var.region_name, var.stage, var.service)
+  description = "Security group for openapi"
+  vpc_id      = module.vpc.vpc_id
+
+  #Essential
+  egress_rules             = ["all-all"]
+
+  ingress_cidr_blocks      = [var.myip]
+  ingress_rules            = ["http-8080-tcp", "ssh-tcp"]
+  # ingress_with_self        = ["all-all"]
+
+  computed_ingress_with_source_security_group_id = [
+    {
+      rule                     = "http-8080-tcp"
+      source_security_group_id = module.loadbalancer_openapi_sg.this_security_group_id
+    }
+  ]
+
+  number_of_computed_ingress_with_source_security_group_id = 1
+
+  tags                     = var.tags #Use common_vars.tf file for tags.Refer Note Below.
+}
+
+    
+module "admin_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = format("%s-%s-%s-%s-admin-sg", var.prefix, var.region_name, var.stage, var.service)
+  description = "Security group for Jenkins"
+  vpc_id      = module.vpc.vpc_id
+
+  #Essential
+  egress_rules             = ["all-all"]
+
+  ingress_cidr_blocks      = [var.myip]
+  ingress_rules            = ["https-443-tcp", "http-8080-tcp", "ssh-tcp"]
+  
+  tags                     = var.tags
+}
