7bf00541d4039a0c469e5d118f830cff3458d435
NokkuPrudhvi2Main <49380061+NokkuPrudhvi@users.noreply.github.com>
2020-05-09 20:15:43 +0530

    Create backup_plan_ec2.tf
    takes backup of ec2

---
+++ backup_plan_ec2.tf
@@ -0,0 +1,57 @@
+resource "aws_backup_plan" "example" {
+  name = "tf_example_backup_plan"
+
+  rule {
+    rule_name         = "tf_example_backup_rule"
+    target_vault_name = "Default"
+    schedule          = "cron(0 12 * * ? *)"
+    #schedule           = "cron(0 15 ? * * *)"
+    #start_window       = 480
+    #completion_window  = 600
+    lifecycle           {
+      #cold_storage_after = 14
+      delete_after = 3
+    }
+  }
+}
+
+
+
+#########################
+
+resource "aws_iam_role" "example" {
+  name               = "example"
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": ["sts:AssumeRole"],
+      "Effect": "allow",
+      "Principal": {
+        "Service": ["backup.amazonaws.com"]
+      }
+    }
+  ]
+}
+POLICY
+}
+
+
+resource "aws_iam_role_policy_attachment" "example" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup"
+  role       = aws_iam_role.example.name
+}
+
+####################
+
+resource "aws_backup_selection" "example" {
+  iam_role_arn = aws_iam_role.example.arn
+  name         = "tf_example_backup_selection"
+  plan_id      = aws_backup_plan.example.id
+
+  resources = [
+    module.batch.arn[0]
+
+  ]
+}
