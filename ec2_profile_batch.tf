499b09b8e99131b6073cfcbb86f4ff275f1433d1
NokkuPrudhvi2Main <49380061+NokkuPrudhvi@users.noreply.github.com>
2020-05-09 23:28:53 +0530

    Create ec2_profile_batch.tf
    added profile for batch as itneeds ec2 permissions for packer

---
+++ ec2_profile_batch.tf
@@ -0,0 +1,46 @@
+#Role for EC2
+resource "aws_iam_role" "ec2_role_batch" {
+  name = format("%s-%s-%s-%s-batch-role", var.prefix, var.region_name, var.stage, var.service)
+
+  assume_role_policy = <<EOF
+{
+"Version": "2012-10-17",
+"Statement": [
+    {
+    "Effect": "Allow",
+    "Principal": {
+        "Service": "ec2.amazonaws.com"
+    },
+    "Action": "sts:AssumeRole",
+    "Sid": ""
+    }
+]
+}
+EOF
+}
+
+#Instance profile
+resource "aws_iam_instance_profile" "ec2_instance_profile_batch" {
+  name = format("%s-%s-%s-%s-batch-profile", var.prefix, var.region_name, var.stage, var.service)
+  role =  aws_iam_role.ec2_role_batch.name
+}
+
+###Custom Policies can be defined as per requirement
+resource "aws_iam_role_policy" "ec2_policy_batch" {
+  name = format("%s-%s-%s-%s-batch-policy", var.prefix, var.region_name, var.stage, var.service)
+  role = aws_iam_role.ec2_role_batch.id
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "EC2Permissions",
+            "Effect": "Allow",
+            "Action": "ec2:*",
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
