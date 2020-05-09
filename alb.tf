74e3aca942121bcbaa91840ba76571d153ca71c1
NokkuPrudhvi2Main <49380061+NokkuPrudhvi@users.noreply.github.com>
2020-05-09 20:11:27 +0530

    Update alb.tf
    changed 2-alb's to 1 alb via path based routing

--- alb.tf
+++ alb.tf
@@ -1,4 +1,4 @@
-module "lb_openapi" {
+module "lb_openapi_admin" {
   source  = "terraform-aws-modules/alb/aws"
 
   name               = format("%s-%s-%s-%s-alb-api", var.prefix, var.region_name, var.stage, "gb")
@@ -9,7 +9,7 @@ module "lb_openapi" {
   subnets            = module.vpc.public_subnets
 
   enable_deletion_protection = true
-
+  
   https_listeners = [
     {
       port               = 443
@@ -18,7 +18,7 @@ module "lb_openapi" {
       target_group_index = 0
     }
   ]
-
+  
   target_groups = [
     {
       name_prefix          = "api"
@@ -29,7 +29,7 @@ module "lb_openapi" {
       health_check = {
         enabled             = true
         interval            = 30
-        path                = "/*"
+        path                = "/openapi/*"
         port                = "traffic-port"
         healthy_threshold   = 3
         unhealthy_threshold = 3
@@ -40,8 +40,30 @@ module "lb_openapi" {
       tags = {
         InstanceTargetGroupTag = "jenkins"
       }
-    }
-  ]
+    },
+    {
+       name_prefix          = "admin"
+       backend_protocol     = "HTTP"
+       backend_port         = 8080
+       target_type          = "instance"
+       deregistration_delay = 10
+       health_check = {
+         enabled             = true
+         interval            = 30
+         path                = "/admin/*"
+         port                = "traffic-port"
+         healthy_threshold   = 3
+         unhealthy_threshold = 3
+         timeout             = 6
+         protocol            = "HTTP"
+         matcher             = "200-399"
+       }
+       tags = {
+         InstanceTargetGroupTag = "jenkins"
+       }
+      }
+
+   ]
 
 #   access_logs {
 #     bucket  = "${aws_s3_bucket.lb_logs.bucket}"
@@ -54,79 +76,42 @@ module "lb_openapi" {
   }
 }
 
-module "lb_admin" {
-  source  = "terraform-aws-modules/alb/aws"
 
-  name               = format("%s-%s-%s-%s-alb-adm", var.prefix, var.region_name, var.stage, "gb")
-  internal           = false
-  load_balancer_type = "application"
-  vpc_id             = module.vpc.vpc_id
-  security_groups    = [module.loadbalancer_admin_sg.this_security_group_id]
-  subnets            = module.vpc.public_subnets
+################################
+resource "aws_lb_listener_rule" "openapi" {
+  listener_arn = module.lb_openapi_admin.https_listener_arns[0]
+  priority     = 99
 
-  enable_deletion_protection = true
-
-  https_listeners = [
-    {
-      port               = 443
-      protocol           = "HTTPS"
-      certificate_arn    = data.aws_acm_certificate.domain.arn
-      target_group_index = 0
-    }
-  ]
-
-  target_groups = [
-    # {
-    #   name_prefix          = "jenkin"
-    #   backend_protocol     = "HTTP"
-    #   backend_port         = 8080
-    #   target_type          = "instance"
-    #   deregistration_delay = 10
-    #   health_check = {
-    #     enabled             = true
-    #     interval            = 30
-    #     path                = "/jenkin/*"
-    #     port                = "traffic-port"
-    #     healthy_threshold   = 3
-    #     unhealthy_threshold = 3
-    #     timeout             = 6
-    #     protocol            = "HTTP"
-    #     matcher             = "200-399"
-    #   }
-    #   tags = {
-    #     InstanceTargetGroupTag = "jenkins"
-    #   }
-    # },
-    {
-      name_prefix          = "admin"
-      backend_protocol     = "HTTP"
-      backend_port         = 8080
-      target_type          = "instance"
-      deregistration_delay = 10
-      health_check = {
-        enabled             = true
-        interval            = 30
-        path                = "/admin/*"
-        port                = "traffic-port"
-        healthy_threshold   = 3
-        unhealthy_threshold = 3
-        timeout             = 6
-        protocol            = "HTTP"
-        matcher             = "200-399"
-      }
-      tags = {
-        InstanceTargetGroupTag = "jenkins"
-      }
-    }
-  ]
-
-#   access_logs {
-#     bucket  = "${aws_s3_bucket.lb_logs.bucket}"
-#     prefix  = "test-lb"
-#     enabled = true
-#   }
-
-  tags = {
-    Environment = var.stage
+  action {
+    type             = "forward"
+    target_group_arn = module.lb_openapi_admin.target_group_arns[0]
   }
-}
+
+  condition {
+    path_pattern {
+      values = ["/openapi/*"]
+    }
+  }
+
+ }
+
+
+
+resource "aws_lb_listener_rule" "admin" {
+  listener_arn = module.lb_openapi_admin.https_listener_arns[0]
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = module.lb_openapi_admin.target_group_arns[1]
+  }
+
+  condition {
+    path_pattern {
+      values = ["/admin/*"]
+    }
+  }
+
+ }
+
+                                                      
