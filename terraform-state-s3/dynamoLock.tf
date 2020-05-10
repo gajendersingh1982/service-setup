4646ae4b0b25113b934bf325df8b6f280e8415f3
Gajender Singh <gajendersingh@gmail.com>
2020-05-10 18:11:43 +0530

    dynamo db name moved to var

--- terraform-state-s3/dynamoLock.tf
+++ terraform-state-s3/dynamoLock.tf
@@ -1,6 +1,6 @@
 #Reference https://blog.gruntwork.io/how-to-manage-terraform-state-28f5697e68fa
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-up-and-running-locks"
+  name         = var.table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
   attribute {
--- terraform-state-s3/variable.tf
+++ terraform-state-s3/variable.tf
@@ -7,6 +7,10 @@ variable "bucket_name" {
   default = "devops-terraform-state-gb"
 }
 
+variable "table_name" {
+  default = "terraform-up-and-running-locks"
+}
+
 
 variable "tags" {
   description = "A mapping of tags to assign to the resource"
