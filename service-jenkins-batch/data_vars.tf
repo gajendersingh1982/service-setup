65e636debd582f459c18f64aee819aa9c3f3c0b2
NokkuPrudhvi2Main <49380061+NokkuPrudhvi@users.noreply.github.com>
2020-05-10 15:55:32 +0530

    Update data_vars.tf
    adding ubuntu16 ami

--- service-jenkins-batch/data_vars.tf
+++ service-jenkins-batch/data_vars.tf
@@ -25,6 +25,32 @@ data "aws_ami" "ubuntu18" {
   }
 }
 
+
+data "aws_ami" "ubuntu16" {
+  most_recent = true
+
+  owners = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-16.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+  
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
 # Define an Amazon Linux AMI.
 data "aws_ami" "amazon_linux" {
   most_recent = true
