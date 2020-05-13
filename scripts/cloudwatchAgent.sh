08bb8ba879329aad4d9a0042f661167242a15b3f
NokkuPrudhvi2Main <49380061+NokkuPrudhvi@users.noreply.github.com>
2020-05-13 06:56:29 +0530

    Update cloudwatchAgent.sh
    working script

--- scripts/cloudwatchAgent.sh
+++ scripts/cloudwatchAgent.sh
@@ -1,20 +1,19 @@
-#!/usr/bin/env bash
-
 curl https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py -O
-sudo python3 ./awslogs-agent-setup.py --region us-east-1 --non-interactive
-sudo rm /var/awslogs/etc/awslogs.conf
-sudo echo "
+
+echo "
 [general]
 # Path to the CloudWatch Logs agent's state file. The agent uses this file to maintain
 # client side state across its executions.
 state_file = /var/awslogs/state/agent-state
-  
+
 [/var/log/syslog]
 buffer_duration = 5000
 initial_position = start_of_file
 datetime_format = %b %d %H:%M:%S
-log_group_name = /var/log/syslog
+log_group_name = /var/log/syslog2
 file = /var/log/syslog
 log_stream_name = {instance_id}
-">/var/awslogs/etc/awslogs.conf
+">configFileForAgent
 
+
+sudo python3 ./awslogs-agent-setup.py --region us-east-1 --non-interactive --configfile configFileForAgent
