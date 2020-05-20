c3fa37df7ce423bb8ef1180cb98e4ba5285ffde8
NokkuPrudhvi2Main <49380061+NokkuPrudhvi@users.noreply.github.com>
2020-05-20 22:48:25 +0530

    Create ouputs.tf

---
+++ service-constant/ouputs.tf
@@ -0,0 +1,41 @@
+#output "openapi__http_listner_arns" {
+#  description = "List of http listner arns"
+#  value       = module.lb_openapi.http_tcp_listener_arns
+#}
+
+output "openapi_https_listener_arns" {
+  description = "List of https listner arns"
+  value       = module.lb_openapi.https_listener_arns
+}
+
+output "openapi_target_group_arns" {
+  description = "List of target group arns"
+  value       = module.lb_openapi.target_group_arns
+}
+
+
+output "openapi_this_lb_arn" {
+  description = "LoadBalancer arn"
+  value       = module.lb_openapi.this_lb_arn
+}
+
+#output "admin__http_listner_arns" {
+#  description = "List of http listner arns"
+#  value       = module.lb_admin.http_tcp_listener_arns
+#}
+
+output "admin_https_listener_arns" {
+  description = "List of https listner arns"
+  value       = module.lb_admin.https_listener_arns
+}
+
+output "admin_target_group_arns" {
+  description = "List of target group arns"
+  value       = module.lb_admin.target_group_arns
+}
+
+
+output "admin_this_lb_arn" {
+  description = "LoadBalancer arn"
+  value       = module.lb_admin.this_lb_arn
+}
